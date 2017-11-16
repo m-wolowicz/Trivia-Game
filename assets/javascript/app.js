46a2337b9c894347dcd9e2a908c82199f7c28d8f
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2017-11-16 12:35:25 -0500

    Created HTML and css only and began to pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+/* 
+GAME STRUCTURE:
+===============
+01. The game starts when the player hits the START button.
+02. Once the game starts the is a clock countdown.
+03. The time given is the total time to finish the whole game.
+04. The game ends if the time runs out, Game Over.
+05. The player can only guess one answer per question.
+06. Include the timer so the player can see it.
+07. All the questions are displayed at once.
+08. There is a DONE button at the end, if the user is finished before the timer stops.
+*/
+
+// GLOBAL VARIABLES
+// ================
+
+	
+
+// FUNTIONS
+// ========
+
+
+// MAIN PROCESS
+//=============
